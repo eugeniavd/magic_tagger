0ba16c044fb4014499a5863d943540f48dcde5b9
Evgeniia Vdovichenko <evgeniia.vdovichenko@studio.unibo.com>
2026-01-28 19:29:26 +0100

    src init

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+# src/__init__.py
+"""
+magic_tagger.src
+
+This file makes `src` a regular Python package so imports like
+`from src.service import classify` work reliably both locally and on
+Streamlit Community Cloud.
+"""
+
+__all__ = []
